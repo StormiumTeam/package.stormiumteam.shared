a0fd2bb9a1640e780fe5bb47919154d0da74f7b2
Guerro323 <kaltobattle@gmail.com>
2018-08-10 22:03:09 +0200

    Added some extensions for bounds and cameras

---
+++ Scripts/Utilities/BoundsExtensions.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Scripts.Utilities
+{
+    public static class BoundsExtensions
+    {
+        public static bool Contains(this Bounds b1, Bounds b2)
+        {
+            return b1.Contains(b2.min) && b1.Contains(b2.max);
+        }
+
+        public static void ApplyFlat2D(this Bounds b1)
+        {
+            var center  = b1.center;
+            var extents = b1.extents;
+
+            center.z  = 0;
+            extents.z = 0;
+            
+            b1.center  = center;
+            b1.extents = extents;
+        }
+        
+        public static Bounds Flat2D(this Bounds b1)
+        {
+            var center  = b1.center;
+            var extents = b1.extents;
+
+            center.z  = 0;
+            extents.z = 0;
+            
+            b1.center  = center;
+            b1.extents = extents;
+
+            return b1;
+        }
+    }
+}
