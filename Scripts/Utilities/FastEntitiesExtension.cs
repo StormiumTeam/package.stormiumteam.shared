d02b701ee38fa02a85d8dafa28f7fbebf7f48dbc
Guerro323 <kaltobattle@gmail.com>
2018-11-19 13:27:10 +0100

    Fixed the zero size issue when calling SetOrAddComponentData
    and also added Addressables package in the assembly def file

--- Scripts/Utilities/FastEntitiesExtension.cs
+++ Scripts/Utilities/FastEntitiesExtension.cs
@@ -68,5 +68,17 @@ namespace package.stormiumteam.shared
             if (em.HasComponent<T>(entity)) em.SetSharedComponentData(entity, data);
             else em.AddSharedComponentData(entity, data);
         }
+        
+        public static void SetOrAddComponentData<T>(this EntityManager em, Entity entity, T data)
+            where T : struct, IComponentData
+        {
+            if (em.HasComponent<T>(entity))
+            {
+                if (TypeManager.GetTypeInfo<T>().IsZeroSized) return;
+                
+                em.SetComponentData(entity, data);
+            }
+            else em.AddComponentData(entity, data);
+        }
     }
 }
