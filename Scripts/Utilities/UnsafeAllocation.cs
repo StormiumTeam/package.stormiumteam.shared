14d9219990e37f8faf3022d5747b22688195d52a
Guerro323 <kaltobattle@gmail.com>
2019-02-04 11:39:30 +0100

    Added UnsafeAllocationLength<T>

--- Scripts/Utilities/UnsafeAllocation.cs
+++ Scripts/Utilities/UnsafeAllocation.cs
@@ -23,4 +23,22 @@ namespace package.stormiumteam.shared
             UnsafeUtility.Free(Data, Allocator);
         }
     }
+    
+    public unsafe struct UnsafeAllocationLength<T> : IDisposable
+        where T : struct
+    {
+        public void*     Data;
+        public Allocator Allocator;
+        
+        public UnsafeAllocationLength(Allocator allocator, int length)
+        {
+            Allocator = allocator;
+            Data      = UnsafeUtility.Malloc(length, UnsafeUtility.AlignOf<T>(), allocator);
+        }
+        
+        public void Dispose()
+        {
+            UnsafeUtility.Free(Data, Allocator);
+        }
+    }
 }
